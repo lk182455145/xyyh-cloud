646b08abe5015c83ac403f58e5b9c788ac280dc9
xz <ldwqh0@outlook.com>
2017-12-30 00:29:17 +0800

    修改部分问题

--- event-bus/src/main/java/com/xyyh/cloud/bus/consumer/Consumer.java
+++ event-bus/src/main/java/com/xyyh/cloud/bus/consumer/Consumer.java
@@ -1,6 +1,5 @@
 package com.xyyh.cloud.bus.consumer;
 
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +10,6 @@ public class Consumer {
 	 * 定义一个消费者
 	 * @param message
 	 */
-	@RabbitHandler
 	@RabbitListener(queues = "good")
 	public void handler(String message) {
 		System.out.println("recive message from " + message);
