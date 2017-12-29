700d51374fe9e21c8ce0942d12bbddddd3b356cc
xz <ldwqh0@outlook.com>
2017-12-29 12:01:58 +0800

    增加event-bus模块

---
+++ event-bus/src/main/java/com/xyyh/cloud/bus/BusApplication.java
@@ -0,0 +1,11 @@
+package com.xyyh.cloud.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BusApplication.class, args);
+	}
+}
---
+++ event-bus/src/main/java/com/xyyh/cloud/bus/events/TestEvent.java
@@ -0,0 +1,8 @@
+package com.xyyh.cloud.bus.events;
+
+import org.springframework.cloud.bus.event.RemoteApplicationEvent;
+
+public class TestEvent extends RemoteApplicationEvent {
+	private static final long serialVersionUID = -4986018828922782798L;
+
+}
